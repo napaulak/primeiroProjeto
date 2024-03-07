0bc068edb05a81743c2fd43b211b46f42e9fe54e
napaulak <anapaulaferreira1b1@gmail.com>
2024-03-07 10:47:54 -0300

    Rota

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './src/components';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Ana Paula Ferreira</Text>
       <Text>Desenvolvimento de dispositivos móveis - 22 fev. 2024</Text>
+     <Title/>
       <StatusBar style="auto" />
     </View>
   
